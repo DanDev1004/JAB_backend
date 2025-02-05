9d0c8b25f322cf5b1d89b4e4365ce7f4f4e056eb
DanDev1004 <dandeveloper1004@gmail.com>
2025-02-05 02:15:24 -0500

    actualizando los tipos de retorno

--- src/company-management/domain/interfaces/group.interface.ts
+++ src/company-management/domain/interfaces/group.interface.ts
@@ -2,11 +2,11 @@ import { Group } from "../group";
 
 export interface IGroupRepository {
     addGroup(group: Group): Promise<Group>;
-    getGroupById(id: number): Promise<Group>;
-    editGroup(id: number, updateData: Partial<Group>): Promise<Group>;
+    getGroupById(id: number): Promise<Group | null>;
+    editGroup(id: number, updateData: Partial<Group>): Promise<Group | 'NOT_FOUND' | 'DELETED'>;
     getAllGroups(): Promise<Group[]>; // Activos y no eliminados
     getAllInactiveGroups(): Promise<Group[]>; // Inactivos pero no eliminados
-    deactivateGroup(id: number): Promise<Group>;
-    activateGroup(id: number): Promise<Group>;
-    logicalGroupDeletion(id: number): Promise<Group>;
+    deactivateGroup(id: number): Promise<Group | 'NOT_FOUND' | 'DELETED'>;
+    activateGroup(id: number): Promise<Group | 'NOT_FOUND' | 'DELETED'>;
+    logicalGroupDeletion(id: number): Promise<Group | 'NOT_FOUND' | 'ACTIVE'>;
 }
